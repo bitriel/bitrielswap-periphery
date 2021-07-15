0156fcee47decdf69f4e7f0a49b4d94d2f3aea91
leang27 <52003343+leang27@users.noreply.github.com>
2021-07-15 21:09:52 +0700

    update Multicall contract

--- contracts/base/Multicall.sol
+++ contracts/base/Multicall.sol
@@ -2,20 +2,28 @@
 pragma solidity >=0.7.6;
 pragma abicoder v2;
 
-import "@openzeppelin/contracts/utils/Address.sol";
 import '../interfaces/IMulticall.sol';
 
 /**
  * @dev Provides a function to batch together multiple calls in a single external call.
  */
 abstract contract Multicall is IMulticall {
-    /**
-     * @dev Receives and executes a batch of function calls on this contract.
-     */
+    /// @inheritdoc IMulticall
     function multicall(bytes[] calldata data) external payable override returns (bytes[] memory results) {
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
-            results[i] = Address.functionDelegateCall(address(this), data[i]);
+            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+
+            if (!success) {
+                // Next 5 lines from https://ethereum.stackexchange.com/a/83577
+                if (result.length < 68) revert();
+                assembly {
+                    result := add(result, 0x04)
+                }
+                revert(abi.decode(result, (string)));
+            }
+
+            results[i] = result;
         }
         return results;
     }
