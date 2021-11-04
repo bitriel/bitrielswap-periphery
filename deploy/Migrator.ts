519dfb2efb7b36a1853348207da604bb991ae9f1
leang27 <52003343+leang27@users.noreply.github.com>
2021-11-04 14:36:31 +0700

    changed migrator old factory address, redeployed

--- deploy/Migrator.ts
+++ deploy/Migrator.ts
@@ -5,7 +5,7 @@ import { ChainId, WNATIVE, FACTORY_ADDRESS } from '@bitriel/bitrielswap-sdk'
 const OLD_FACTORY_MAP: {[chainId in ChainId | number]: string} = {
   [ChainId.MAINNET]: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
   [ChainId.BSC]: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
-  [ChainId.BSC_TESTNET]: "0x927A548cD60c0A4b410c32178A88454A70B22fea"
+  [ChainId.BSC_TESTNET]: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
 }
 
 const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
